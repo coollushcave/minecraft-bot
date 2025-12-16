feb658520628abbe755f225148722c9281f91c3e
coollushcave <blueaxolotl213@gmail.com>
2025-12-16 12:34:20 +0700

    Create java.js

---
+++ java.js
@@ -0,0 +1,36 @@
+const mc = require('minecraft-protocol');
+
+const CONFIG = {
+  host: "example.server.ip", // Server IP
+  port: 25565, // port
+  username: "email", // Your minecraft email
+  auth: "microsoft", // Auth
+  afkIntervalMs: 1000 // Config
+};
+
+function startBot() {
+  console.log("Starting bot");
+
+  const bot = mc.createClient({
+    host: CONFIG.host,
+    port: CONFIG.port,
+    username: CONFIG.username,
+    auth: CONFIG.auth,
+    version: "1.21.x" minecraft version
+  });
+
+  bot.on("login", () => {
+    console.log("Logged in to server!");
+  });
+
+  bot.on("end", () => {
+    console.log("Disconnected! Reconnecting in 5 seconds.");
+    setTimeout(startBot, 5000); // timer
+  });
+
+  bot.on("error", (err) => {
+    console.log("[ERROR]", err.toString());
+  });
+}
+
+startBot();
